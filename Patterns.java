489f7dfdaad4855ae73a54f7137084c952621b2a
kartik01112004 <kartikgoel018@gmail.com>
2024-07-17 23:06:15 +0530

    trying advanced patterns

--- Patterns.java
+++ Patterns.java
@@ -27,14 +27,67 @@ public class Patterns {
         //     System.out.println();
         // }
         // Char pattern
-        char ch = 'A';
-        for (int line = 1; line <= n; line++) {
-            for (int chars = 1; chars <= line; chars++) {
-                System.out.print(ch);
-                ch++;
-            }
-            System.out.println();
-        }
+        // char ch = 'A';
+        // for (int line = 1; line <= n; line++) {
+        //     for (int chars = 1; chars <= line; chars++) {
+        //         System.out.print(ch);
+        //         ch++;
+        //     }
+        //     System.out.println();
+        // }
+        // int height = 4;
+        // int width = 5;
+        // for (int i = 1; i <= height; i++) {
+        //     for (int j = 1; j <= width; j++) {
+        //         if (i == 1 || i == 4) {
+        //             System.out.print("* ");
+        //         } else {
+        //             if (j == 1 || j == 5) {
+        //                 System.out.print("* ");
+        //             } else {
+        //                 System.out.print("  ");
+        //             }
+        //         }
+        //     }
+        //     System.out.println();
+        // }
+        // int line = 4;
+        // for (int i = 1; i <= line; i++) {
+        //     for (int k = 1; k <= line - i; k++) {
+        //         System.out.print(" ");
+        //     } 
+        //     for (int j = 4; j > line - i; j--) {
+        //         System.out.print("*");
+        //     }
+        //     System.out.println();
+        // }
+        // for (int i = 5; i >= 1; i--) {
+        //     for (int j = 1; j <= i; j++) {
+        //         System.out.print(j);
+        //     }
+        //     System.out.println();
+        // }
+        // int line = 5;
+        // int num = 1;
+        // for (int i = 1; i <= line; i++) {
+        //     for (int j = 1; j <= i; j++) {
+        //         System.out.print(num + " ");
+        //         num++;
+        //     }
+        //     System.out.println();
+        // }
+        //// int line = 5;
+        //// for (int i = 1; i <= line; i++) {
+        ////   for (int j = 1; j <= i; j++) {
+        ////     if (((i % 2) == 0) || ((j % 2) == 0)) {
+        ////             System.out.print("0");
+        ////         } else {
+        ////             System.out.print("1");
+        ////         }
+        ////     }
+        ////    System.out.println();
+        //// }
 
+        
     }
 }
